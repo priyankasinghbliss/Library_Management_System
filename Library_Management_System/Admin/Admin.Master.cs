973c9d33df6c73c6f9bdade7508796ec8c38a166
priyankasinghbliss <priyanka.23.12.1999@gmail.com>
2025-02-09 21:17:14 +0530

    added the master page for admin

---
+++ Library_Management_System/Admin/Admin.Master.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Library_Management_System.Admin
+{
+    public partial class Admin : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            if (string.IsNullOrEmpty((string)Session["Adminrole"]) && Session["Adminrole"].Equals("Admin"))
+            {
+                if (!IsPostBack)
+                {
+                    lblAdminName.Text ="Hi," + Session["Adminfullname"].ToString();
+                }
+            }
+            else
+            {
+                Response.Redirect("../default.aspx");
+            }
+        }
+    }
+}
---
+++ Library_Management_System/Admin/AdminHome.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="Admin Dashboard" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="AdminHome.aspx.cs" Inherits="Library_Management_System.Admin.AdminHome" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h3>Welcome to Admin Dashboard</h3>
+</asp:Content>
